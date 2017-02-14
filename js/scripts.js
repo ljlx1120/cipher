5cb66cf626c5efcdd1d4e5c9beb39821cc0e2640
Xing Li and Zach Beecher <xing@>
2017-02-13 16:58:10 -0800

    uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ var userSentence = prompt("Please type your sentence here");
   var letter2 = getinfo.charAt(getinfo.length-1);
   uppercaseletter1 = letter1.toUpperCase();
   uppercaseletter2 = letter2.toUpperCase();
+  return uppercaseletter1 + uppercaseletter2;
 };
 
 console.log(info(userSentence));
